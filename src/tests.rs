39d086f32e3686408522222d4e3eee7c4d4a4a15
marcasmar1 <121232471+marcasmar1@users.noreply.github.com>
2024-04-04 12:03:38 +0200

    Use nmea crate and add tests (#5)
    * Imported NMEA parser
    
    * Fixed tests
    
    * Shortened 1_broken test
    
    * Update version, add changelog entry
    
    * Run cargo fmt
    
    * Clippy fixes
    
    ---------
    
    Co-authored-by: aleokdev <aleok.inf@gmail.com>

---
+++ src/tests.rs
@@ -0,0 +1,42 @@
+use std::{fs::File, io::BufReader};
+
+use anyhow::Context;
+
+use crate::parse_file;
+
+#[test]
+fn read_correct_file() {
+    let input_path = "tests/assets/1";
+    let file = BufReader::new(
+        File::open(&input_path)
+            .with_context(|| format!("Failed to read input file at {}", input_path))
+            .unwrap(),
+    );
+
+    let _positions = parse_file(file).unwrap();
+}
+
+#[test]
+fn read_blank_file() {
+    let input_path = "tests/assets/2";
+    let file = BufReader::new(
+        File::open(&input_path)
+            .with_context(|| format!("Failed to read input file at {}", input_path))
+            .unwrap(),
+    );
+
+    let _positions = parse_file(file).unwrap();
+}
+
+#[test]
+#[should_panic]
+fn read_broken_file() {
+    let input_path = "tests/assets/1_broken";
+    let file = BufReader::new(
+        File::open(&input_path)
+            .with_context(|| format!("Failed to read input file at {}", input_path))
+            .unwrap(),
+    );
+
+    let _positions = parse_file(file).unwrap();
+}
